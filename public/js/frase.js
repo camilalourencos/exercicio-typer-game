837990a182f252d3ecc0f3769adbd0e410c5c2aa
Camila Lourenço <61992484+camilalourencos@users.noreply.github.com>
2020-10-24 18:59:33 -0300

    commit Função frase aleatória implementada

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,3 +1,14 @@
-$("#botao-frase").click(function(){
-    console.log ("fui clicado");
-})
+// implentar frase aleatória
+$("#botao-frase").click(fraseAleatoria)
+
+    function fraseAleatoria(){
+        $.get("http://localhost:3000/frases", trocaFrase) // o get faz uma requisição para a página informada e, em seguida, executa a função.
+    };
+
+   function trocaFrase(data){ 
+        var frase = $(".frase");
+        var numeroAleatorio = Math.floor(Math.random()*data.length);
+         
+        frase.text(data[numeroAleatorio].texto);
+
+   };
